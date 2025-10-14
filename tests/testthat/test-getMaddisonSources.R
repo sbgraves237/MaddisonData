ead33ec9c0d97ca1463813ce2e3fb5bb17d16ac9
sbgraves237 <spencer.graves@effectivedefense.org>
2025-10-13 20:38:28 -0500

    qualify MaddisonData::...

--- tests/testthat/test-getMaddisonSources.R
+++ tests/testthat/test-getMaddisonSources.R
@@ -2,7 +2,8 @@ test_that("getMaddisonSources", {
   MDPall <- getMaddisonSources() # all 
   MDPonly <- getMaddisonSources(plot=FALSE) # only MDP 
   GBR <- getMaddisonSources('GBR') # GBR 
-  MDPonly12 <- getMaddisonSources(names(MaddisonSources)[1:12], FALSE)#only MDP 
+  MDPonly12 <- getMaddisonSources(
+    names(MaddisonData::MaddisonSources)[1:12], FALSE)#only MDP 
   GBR_USAsince1500 <- getMaddisonSources(data.frame(ISO=c('GBR', 'USA'), 
                 yearBegin=rep(1500, 2)) ) #GBR, USA since 1500 
   GBR_USA1500_1790 <- getMaddisonSources(data.frame(ISO=c('GBR', 'USA'), 
@@ -17,8 +18,8 @@ test_that("getMaddisonSources", {
                 'Conference Board for all countries included in TED.', 
                 'Otherwise UN national accounts statistics') ) )
   MDPall_ <- MDP1 
-  for(iso in names(MaddisonSources)){
-    isoi <- cbind(ISO=iso, MaddisonSources[[iso]])
+  for(iso in names(MaddisonData::MaddisonSources)){
+    isoi <- cbind(ISO=iso, MaddisonData::MaddisonSources[[iso]])
     MDPall_ <- rbind(MDPall_, isoi)
   }
   expect_equal(MDPall, MDPall_)
@@ -29,14 +30,14 @@ test_that("getMaddisonSources", {
       'A new 2023 update", Journal of Economic Surveys, 1-41') )
   expect_equal(MDPonly, MDPonly_)
 # GBR
-  GBR1 <- cbind(ISO='GBR', MaddisonSources[['GBR']])
+  GBR1 <- cbind(ISO='GBR', MaddisonData::MaddisonSources[['GBR']])
   GBR_ <- rbind(MDP1, GBR1)
   expect_equal(GBR, GBR_)
 # MDPonly12
   expect_equal(MDPonly12, MDPonly_)
 # GBR_USAsince1500
   GBRsince1500 <- GBR1[-1,]
-  USA_ <- cbind(ISO='USA', MaddisonSources[['USA']])
+  USA_ <- cbind(ISO='USA', MaddisonData::MaddisonSources[['USA']])
   GBR_USAsince1500_ <- rbind(MDP1, GBRsince1500, USA_)
   row.names(GBR_USAsince1500_) <- NULL
   expect_equal(GBR_USAsince1500, GBR_USAsince1500_) 

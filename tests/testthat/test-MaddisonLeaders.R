3df938020e672dd846467998a0065ec3d4b5c079
sbgraves237 <spencer.graves@effectivedefense.org>
2025-11-05 15:07:39 -0600

    add MaddisonLeaders

---
+++ tests/testthat/test-MaddisonLeaders.R
@@ -0,0 +1,30 @@
+test_that("MaddisonLeaders", {
+  Leaders0 <- MaddisonLeaders() # max GDPpc for each year. 
+  expect_in('data.frame', class(Leaders0))
+  expect_equal(colnames(Leaders0), c('yearBegin', 'yearEnd', 'ISO'))
+  L0NA <- sapply(Leaders0, function(x)sum(is.na(x)))
+  expect_equal(L0NA, c(yearBegin=0, yearEnd=0, ISO=0))
+  expect_gt(length(table(Leaders0$ISO)), 1)
+# attr  
+  LeaderByYear0 <- attr(Leaders0, 'LeaderByYear')
+  expect_in('data.frame', class(LeaderByYear0))
+  expect_equal(colnames(LeaderByYear0), c('year', 'maxGDPpc', 'ISO'))
+  LbY0NA <- sapply(LeaderByYear0, function(x)sum(is.na(x)))
+  expect_equal(LbY0NA, c(year=0, maxGDPpc=0, ISO=0))
+  expect_gt(length(table(LeaderByYear0$ISO)), 1)
+# Presumed technology leaders without commodity leaders with narrow 
+# economies 
+  Leaders1 <- MaddisonLeaders(c('ARE', 'KWT', 'QAT')) 
+  expect_in('data.frame', class(Leaders1))
+  expect_equal(colnames(Leaders1), c('yearBegin', 'yearEnd', 'ISO'))
+  L1NA <- sapply(Leaders1, function(x)sum(is.na(x)))
+  expect_equal(L1NA, c(yearBegin=0, yearEnd=0, ISO=0))
+  expect_gt(length(table(Leaders1$ISO)), 1)
+# attr  
+  LeaderByYear1 <- attr(Leaders1, 'LeaderByYear')
+  expect_in('data.frame', class(LeaderByYear1))
+  expect_equal(colnames(LeaderByYear1), c('year', 'maxGDPpc', 'ISO'))
+  LbY1NA <- sapply(LeaderByYear1, function(x)sum(is.na(x)))
+  expect_equal(LbY1NA, c(year=0, maxGDPpc=0, ISO=0))
+  expect_gt(length(table(LeaderByYear1$ISO)), 1)
+})

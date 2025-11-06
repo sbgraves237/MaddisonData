5110e7d1323c31bf91b7d1441f4f9ad1748bb59a
sbgraves237 <spencer.graves@effectivedefense.org>
2025-11-05 21:22:26 -0600

    add cols dy0, dy1

--- tests/testthat/test-MaddisonLeaders.R
+++ tests/testthat/test-MaddisonLeaders.R
@@ -2,9 +2,10 @@ test_that("MaddisonLeaders", {
   Leaders0 <- MaddisonLeaders() # max GDPpc for each year. 
   expect_in('data.frame', class(Leaders0))
   expect_equal(colnames(Leaders0), c(
-    'yearBegin', 'yearEnd', 'gdppc0', 'gdppc1', 'ISO'))
+    'yearBegin', 'yearEnd', 'gdppc0', 'gdppc1', 'ISO', 'dy0','dy1'))
   L0NA <- sapply(Leaders0, function(x)sum(is.na(x)))
-  expect_equal(L0NA, c(yearBegin=0, yearEnd=0, gdppc0=0, gdppc1=0, ISO=0))
+  expect_equal(L0NA, c(yearBegin=0, yearEnd=0, gdppc0=0, gdppc1=0, ISO=0, 
+                       dy0=0, dy1=1))
   expect_gt(length(table(Leaders0$ISO)), 1)
 # attr  
   LeaderByYear0 <- attr(Leaders0, 'LeaderByYear')
@@ -20,9 +21,10 @@ test_that("MaddisonLeaders", {
   Leaders1 <- MaddisonLeaders(c('ARE', 'KWT', 'QAT')) 
   expect_in('data.frame', class(Leaders1))
   expect_equal(colnames(Leaders1), c(
-    'yearBegin', 'yearEnd', 'gdppc0', 'gdppc1', 'ISO'))
+    'yearBegin', 'yearEnd', 'gdppc0', 'gdppc1', 'ISO', 'dy0', 'dy1'))
   L1NA <- sapply(Leaders1, function(x)sum(is.na(x)))
-  expect_equal(L1NA, c(yearBegin=0, yearEnd=0, gdppc0=0, gdppc1=0, ISO=0))
+  expect_equal(L1NA, c(yearBegin=0, yearEnd=0, gdppc0=0, gdppc1=0, ISO=0, 
+                       dy0=0, dy1=1))
   expect_gt(length(table(Leaders1$ISO)), 1)
 # attr  
   LeaderByYear1 <- attr(Leaders1, 'LeaderByYear')

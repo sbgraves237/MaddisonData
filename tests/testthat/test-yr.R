e442e4c2305a33b60e0de5f09cd303c852bf4b53
sbgraves237 <spencer.graves@effectivedefense.org>
2025-12-05 10:43:38 -0600

    impr test-yr

--- tests/testthat/test-yr.R
+++ tests/testthat/test-yr.R
@@ -1,7 +1,8 @@
 test_that("yr", {
   Jan2_24_25 <- c('2024-01-02', '2025-01-02')
   J2yr <- yr(Jan2_24_25)
-  expect_true(inherits(J2yr,"numeric"))
+  J2y_ <- (c(2024, 2025) + (1/c(366,365)))
+  expect_identical(J2yr, J2y_)
   J2y <- yr(as.POSIXct(Jan2_24_25))
-  expect_equal(J2yr, J2y)
+  expect_identical(J2yr, J2y)
 })

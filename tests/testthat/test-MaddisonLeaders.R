94ef42310676c7447af06c8ef47cb1974b046bef
sbgraves237 <spencer.graves@effectivedefense.org>
2025-12-25 12:07:24 -0600

    refactor

--- tests/testthat/test-MaddisonLeaders.R
+++ tests/testthat/test-MaddisonLeaders.R
@@ -2,10 +2,10 @@ test_that("MaddisonLeaders", {
   Leaders0 <- MaddisonLeaders() # max GDPpc for each year. 
   expect_in('MaddisonLeaders', class(Leaders0))
   expect_equal(colnames(Leaders0), c(
-    'yearBegin', 'yearEnd', 'gdppc0', 'gdppc1', 'ISO', 'dy0','dy1'))
+    'yearBegin', 'yearEnd', 'gdppc0', 'gdppc1', 'ISO', 'dyear0','dyear1'))
   L0NA <- sapply(Leaders0, function(x)sum(is.na(x)))
   expect_equal(L0NA, c(yearBegin=0, yearEnd=0, gdppc0=0, gdppc1=0, ISO=0, 
-                       dy0=0, dy1=1))
+                       dyear0=0, dyear1=1))
   expect_gt(length(table(Leaders0$ISO)), 1)
 # attr  
   LeaderByYear0 <- attr(Leaders0, 'LeaderByYear')
@@ -21,10 +21,10 @@ test_that("MaddisonLeaders", {
   Leaders1 <- MaddisonLeaders(c('ARE', 'KWT', 'QAT')) 
   expect_in('MaddisonLeaders', class(Leaders1))
   expect_equal(colnames(Leaders1), c(
-    'yearBegin', 'yearEnd', 'gdppc0', 'gdppc1', 'ISO', 'dy0', 'dy1'))
+    'yearBegin', 'yearEnd', 'gdppc0', 'gdppc1', 'ISO', 'dyear0', 'dyear1'))
   L1NA <- sapply(Leaders1, function(x)sum(is.na(x)))
   expect_equal(L1NA, c(yearBegin=0, yearEnd=0, gdppc0=0, gdppc1=0, ISO=0, 
-                       dy0=0, dy1=1))
+                       dyear0=0, dyear1=1))
   expect_gt(length(table(Leaders1$ISO)), 1)
 # attr  
   LeaderByYear1 <- attr(Leaders1, 'LeaderByYear')
@@ -39,4 +39,21 @@ test_that("MaddisonLeaders", {
   MadDat1600 <- subset(MaddisonData, year>1600)
   Leaders1600 <- MaddisonLeaders(c('ARE', 'KWT', 'QAT'), data=MadDat1600)
   expect_gt(min(Leaders1600$yearBegin), 1600)
+#
+# max pop by region within percentiles of gdppc
+  noGDP <- is.na(MaddisonData$gdppc)
+  MadDat <-MaddisonData[!noGDP, ]
+  gdpPcts <- quantile(MadDat$gdppc, seq(0, 1, .01), na.rm=TRUE)
+  gdpPct <- unique(as.numeric(gdpPcts[-1]))
+  gdpPc <-c(gdpPct[-100], tail(gdpPct, 1)*(1+sqrt(.Machine$double.eps)))
+  gdp100 <- MadDat$gdppc
+  nObs <- nrow(MadDat)
+  for(i in 1:nObs){
+    gdp100[i] <- min(gdpPc[MadDat$gdppc[i]<gdpPc])
+  }
+  MadDat$gdp100 <- gdp100
+  MadDat$region <- MaddisonCountries[MadDat$ISO, 'region', drop=TRUE]
+  MadPopRgnGDP <- MaddisonLeaders(y='pop', group='region', data=MadDat,
+                                  x='gdp100')
+  expect_in('MaddisonLeaders', class(MadPopRgnGDP))  
 })

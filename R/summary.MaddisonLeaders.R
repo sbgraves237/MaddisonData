deb8666e06f5b615a679a6a9c5349843a8c40cab
sbgraves237 <spencer.graves@effectivedefense.org>
2025-12-24 19:57:20 -0600

    doc dots

--- R/summary.MaddisonLeaders.R
+++ R/summary.MaddisonLeaders.R
@@ -3,23 +3,24 @@
 #' `summary.MaddisonLeaders` returns a [`data.frame`] with columns `ISO`, 
 #' `paste0(x, 'Begin)`, `paste0(x, 'End')`, `n`, and `p`. 
 #' 
-#' @param data = object of class `MaddisonLeaders`. 
+#' @param object = object of class `MaddisonLeaders`. 
+#' @param ... = optional arguments for `summary` (not used)
 #' 
 #' @returns a [`data.frame`] with columns 
 #' 
 #' \itemize{ 
-#' \item `ISO` = One row for each level of `ISO` in `unique(data[, 'ISO'])`
-#' \item `paste0(x, 'Begin)` = earliest `data[, paste0(x, 'Begin')]` for `ISO`
-#' \item `paste0(x, 'End')`, last `data[, paste0(x, 'End')]` for `ISO`
+#' \item `ISO` = One row for each level of `ISO` in `unique(object[, 'ISO'])`
+#' \item `paste0(x, 'Begin)` = earliest `object[, paste0(x, 'Begin')]` for `ISO`
+#' \item `paste0(x, 'End')`, last `object[, paste0(x, 'End')]` for `ISO`
 #' \item `n` = sum of `(paste0(x, 'End')` - `paste0(x, 'Begin') + 1` for `ISO`. 
 #' \item `p` = `n/(paste0(x, 'End') - paste0(x, 'Begin') + 1)`.           
 #' })
 #' 
 #' (defaults: 
 #' \itemize{
-#' \item `ISO` = One row for each level of `ISO` in `unique(data[, 'ISO'])`
-#' \item `yearBegin` = earliest `data[, 'yearBegin')]` for `ISO`
-#' \item `yearEnd` = last `data[, 'yearEnd')]` for `ISO`
+#' \item `ISO` = One row for each level of `ISO` in `unique(object[, 'ISO'])`
+#' \item `yearBegin` = earliest `object[, 'yearBegin')]` for `ISO`
+#' \item `yearEnd` = last `object[, 'yearEnd')]` for `ISO`
 #' \item `n` = sum of `('yearEnd' - 'yearBegin' + 1)` for `ISO`. 
 #' \item `p` = `n/(yearEnd - yearBegin + 1)`.            
 #' }
@@ -31,47 +32,47 @@
 #' summary(Leaders0) 
 #' 
 #' @keywords manip 
-summary.MaddisonLeaders <- function(data, ...){
+summary.MaddisonLeaders <- function(object, ...){
 ##
 ## 1. ISOs
 ##   
-  ISOs <- sort(unique(data[, 'ISO']))
+  ISOs <- sort(unique(object[, 'ISO']))
   sumLdrs <- data.frame(ISO=ISOs, yB=NA, yE=NA, n=NA, p=NA)
 ##
 ## 2. find yBegin, yEnd
 ##  
-  iB <- grep('Begin$', names(data))
+  iB <- grep('Begin$', names(object))
   nB <- length(iB)
   if(nB<1){
-    stop('yBegin not found in names(data) = ', 
-         paste(names(data), collapse=', '))
+    stop('yBegin not found in names(object) = ', 
+         paste(names(object), collapse=', '))
   }
   if(nB>1){
-    stop('yBegin found multiple times in names(data) = ', 
-        paste(names(data), collapse=', '))
+    stop('yBegin found multiple times in names(object) = ', 
+        paste(names(object), collapse=', '))
   }
-  names(sumLdrs)[2] <- names(data)[iB]
+  names(sumLdrs)[2] <- names(object)[iB]
 #  
-  iE <- grep('End$', names(data))
+  iE <- grep('End$', names(object))
   nE <- length(iE)
   if(nE<1){
-    stop('yEnd not found in names(data) = ', 
-         paste(names(data), collapse=', '))
+    stop('yEnd not found in names(object) = ', 
+         paste(names(object), collapse=', '))
   }
   if(nE>1){
-    stop('yEnd found multiple times in names(data) = ', 
-        paste(names(data), collapse=', '))
+    stop('yEnd found multiple times in names(object) = ', 
+        paste(names(object), collapse=', '))
   }
-  names(sumLdrs)[3] <- names(data)[iE]
+  names(sumLdrs)[3] <- names(object)[iE]
 ##
 ## 3. for(iSO in ISO)
 ##  
   rownames(sumLdrs) <- ISOs
   for(iSO in ISOs){
-    sel <- (data[, 'ISO'] == iSO) 
-    sumLdrs[iSO, 2] <- min(data[sel, iB])
-    sumLdrs[iSO, 3] <- max(data[sel, iE])
-    sumLdrs[iSO, 'n'] <- sum(data[sel, iE]-data[sel, iB]+1)
+    sel <- (object[, 'ISO'] == iSO) 
+    sumLdrs[iSO, 2] <- min(object[sel, iB])
+    sumLdrs[iSO, 3] <- max(object[sel, iE])
+    sumLdrs[iSO, 'n'] <- sum(object[sel, iE]-object[sel, iB]+1)
     sumLdrs[iSO, 'p'] <- (sumLdrs[iSO, 'n'] / 
                 (sumLdrs[iSO, 3] - sumLdrs[iSO, 2] + 1) )
   }
